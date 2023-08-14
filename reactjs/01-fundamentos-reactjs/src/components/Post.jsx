00ebf98b244f409b85dfc2fd848a9a9e5bd4730c
Jhonatas56 <jhonatas30souza@gmail.com>
2023-08-14 13:21:19 -0300

    added comments

--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -15,24 +15,36 @@ export function Post() {
           </div>
         </div>
 
-        <time title="11 de Agosto as 15 horas" dateTime="2023-08-11 15:21:22">Publicado há 1h</time>
+        <time title="11 de Agosto as 15 horas" dateTime="2023-08-11 15:21:22">
+          Publicado há 1h
+        </time>
       </header>
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
 
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
 
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
 
         <p>
           <a href="">#novoprojeto</a>
-          <a href="">#nlw</a>  
+          <a href="">#nlw</a>
           <a>#rocketseat</a>
         </p>
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe seu comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
       </form>
     </article>
   );
