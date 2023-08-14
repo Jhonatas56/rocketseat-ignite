7b74403a0b6cdc6cf81806ce40f1d1a8c0ad8c57
Jhonatas56 <jhonatas30souza@gmail.com>
2023-08-14 16:31:15 -0300

    tweaked post and main page component

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -6,23 +6,62 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Jhonatas56.png",
+      name: "Jhonatas Jorge",
+      role: "Programador",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-08-14 15:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/teste.png",
+      name: "Joaozinho",
+      role: "Programador",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-08-13 15:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Jhonatas Jorge"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quas iusto repudiandae repellat exercitationem, ratione nam, fuga magni aspernatur doloremque, minus cum molestiae deleniti laudantium nemo sapiente omnis. Nisi, porro!"
-          />
-
-          <Post
-            author="Jhonatas Jorge"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quas iusto repudiandae repellat exercitationem, ratione nam, fuga magni aspernatur doloremque, minus cum molestiae deleniti laudantium nemo sapiente omnis. Nisi, porro!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
