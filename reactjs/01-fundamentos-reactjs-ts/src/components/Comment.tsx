998ef711573fac6fcd0d060a25039701f37aa69c
Jhonatas56 <jhonatas30souza@gmail.com>
2023-08-15 15:34:15 -0300

    first commit

---
+++ reactjs/01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,53 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(state => {
+      return state+1 
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Jhonatas56.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Jhonatas Jorge</strong>
+              <time
+                title="11 de Agosto as 15 horas"
+                dateTime="2023-08-11 15:21:22"
+              >
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ reactjs/01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import { Avatar } from './Avatar';
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1663040543387-cb7c78c4f012?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/Jhonatas56.png" />
+        <strong>Jhonatas Jorge</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
