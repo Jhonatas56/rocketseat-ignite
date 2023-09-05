a5252dec82fd1e452581bb8c8173aa3bb5f3d978
Jhonatas56 <jhonatas30souza@gmail.com>
2023-09-05 18:46:55 -0300

    feat: create default theme

--- reactjs/02-ignite-timer/src/Button.styles.ts
+++ reactjs/02-ignite-timer/src/Button.styles.ts
@@ -16,6 +16,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${({variant}) => `background: ${buttonVariants[variant]}`}
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  
+  /* ${({variant}) => `background: ${buttonVariants[variant]}`} */
 `;
